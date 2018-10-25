8b487755ff472f4ad99ab42d6d6b463a1b9a6336
Lucas Zhang <c21lz@dalton.org>
2018-10-25 15:51:05 -0400

    version control test
    Signed-off-by: Lucas Zhang <c21lz@dalton.org>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareUpChassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareUpChassis.java
@@ -51,7 +51,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Servo channel:  Servo to open left claw:  "left_hand"
  * Servo channel:  Servo to open right claw: "right_hand"
  */
-public class HardwareUpChassis
+public class HardwareUpChassis //hello it's lucas
 {
     /* Public OpMode members. */
     public DcMotor  frontleft   = null;
